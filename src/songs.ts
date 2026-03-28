c6cf7675c624e7d026b7f3ac6d11216fc234fec4
hamalainenmiska100-hue <hamalainen.miska100@gmail.com>
2026-03-28 15:56:57 +0200

    Create Ore-themed React TypeScript iOS Minecraft song player

---
+++ src/songs.ts
@@ -0,0 +1,22 @@
+export type Song = {
+  id: number;
+  title: string;
+  url: string;
+};
+
+export const SONGS: Song[] = [
+  { id: 1, title: 'Key', url: 'https://dn710204.ca.archive.org/0/items/08-minecraft_202302/01%20-%20Key.mp3' },
+  { id: 2, title: 'Door', url: 'https://dn710204.ca.archive.org/0/items/08-minecraft_202302/02%20-%20Door.mp3' },
+  { id: 3, title: 'Subwoofer Lullaby', url: 'https://dn710204.ca.archive.org/0/items/08-minecraft_202302/03%20-%20Subwoofer%20Lullaby.mp3' },
+  { id: 4, title: 'Death', url: 'https://dn710204.ca.archive.org/0/items/08-minecraft_202302/04%20-%20Death.mp3' },
+  { id: 5, title: 'Living Mice', url: 'https://dn710204.ca.archive.org/0/items/08-minecraft_202302/05%20-%20Living%20Mice.mp3' },
+  { id: 6, title: 'Moog City', url: 'https://dn710204.ca.archive.org/0/items/08-minecraft_202302/06%20-%20Moog%20City.mp3' },
+  { id: 7, title: 'Haggstrom', url: 'https://dn710204.ca.archive.org/0/items/08-minecraft_202302/07%20-%20Haggstrom.mp3' },
+  { id: 8, title: 'Minecraft', url: 'https://ia801607.us.archive.org/17/items/08-minecraft_202302/08%20-%20Minecraft.mp3' },
+  { id: 9, title: 'Oxygène', url: 'https://dn710204.ca.archive.org/0/items/08-minecraft_202302/09%20-%20Oxygène.mp3' },
+  { id: 10, title: 'Équinoxe', url: 'https://dn710204.ca.archive.org/0/items/08-minecraft_202302/10%20-%20Équinoxe.mp3' },
+  { id: 11, title: 'Mice on Venus', url: 'https://dn710204.ca.archive.org/0/items/08-minecraft_202302/11%20-%20Mice%20on%20Venus.mp3' },
+  { id: 12, title: 'Dry Hands', url: 'https://dn710204.ca.archive.org/0/items/08-minecraft_202302/12%20-%20Dry%20Hands.mp3' },
+  { id: 13, title: 'Wet Hands', url: 'https://dn710204.ca.archive.org/0/items/08-minecraft_202302/13%20-%20Wet%20Hands.mp3' },
+  { id: 14, title: 'Clark', url: 'https://dn710204.ca.archive.org/0/items/08-minecraft_202302/14%20-%20Clark.mp3' }
+];
